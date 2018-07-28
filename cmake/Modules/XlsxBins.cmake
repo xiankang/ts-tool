6be51b36074ce3795b9486ee0363cd78c32311d0
chenxiankang <wwwxiankang@163.com>
2018-07-28 15:31:58 +0800

    xlsx改成dll动态库

--- cmake/Modules/XlsxBins.cmake
+++ cmake/Modules/XlsxBins.cmake
@@ -6,3 +6,34 @@ macro(use_xlsx_module _target)
   target_link_libraries(${_target} xlsx)
   include_directories(${XLSX_INCLUDE_DIRS})
 endmacro()
+
+set(xlsx_Debug_path
+  ${CMAKE_BINARY_DIR}/xlsx/Debug/
+)
+set(xlsx_Release_path
+  ${CMAKE_BINARY_DIR}/xlsx/Release/
+)
+
+function(xlsx_helper_install_dir target target_configs pattern source dest)
+	add_custom_command(TARGET ${target} POST_BUILD
+  COMMAND "${CMAKE_COMMAND}"
+			"-DCONFIG=$<CONFIGURATION>"
+      "-DTARGET_CONFIGS=${target_configs}"
+      "-DPATTERN=${pattern}"
+			"-DINPUT=${source}"
+			"-DOUTPUT=${dest}"
+			-P "${CMAKE_SOURCE_DIR}/cmake/install_helper.cmake"
+		VERBATIM)
+endfunction()
+
+function(install_xlsx_bin target)
+  xlsx_helper_install_dir(${target} "Release;MinSizeRel;RelWithDebInfo"
+    ".*.dll"
+    "${xlsx_Release_path}"
+    "${CMAKE_RUNTIME_OUTPUT_DIRECTORY_RELEASE}")
+  xlsx_helper_install_dir(${target} "Debug"
+    ".*.dll|.*.pdb"
+    "${xlsx_Debug_path}"
+    "${CMAKE_RUNTIME_OUTPUT_DIRECTORY_DEBUG}")
+endfunction()
+
---
+++ cmake/install_helper.cmake
@@ -0,0 +1,23 @@
+
+if(NOT EXISTS "${INPUT}")
+return()
+endif()
+
+set(_do_pass FALSE)
+foreach(target ${TARGET_CONFIGS})
+if(target STREQUAL "${CONFIG}" OR target STREQUAL "ALL")
+    set(_do_pass TRUE)
+endif()
+endforeach()
+
+if(NOT _do_pass)
+return()
+endif()
+
+message(${INPUT})
+message(${OUTPUT})
+message(${PATTERN})
+file(INSTALL "${INPUT}" 
+    DESTINATION "${OUTPUT}" 
+    USE_SOURCE_PERMISSIONS 
+    FILES_MATCHING REGEX "${PATTERN}")
